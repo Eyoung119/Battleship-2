fed13c19b8118a0a65dc142a04981d4283cd6042
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-02-28 14:56:23 -0700

    Update Battleship2.java

--- controller/Battleship2.java
+++ controller/Battleship2.java
@@ -4,8 +4,8 @@ import models.Board;
 import models.Player;
 
 public class Battleship2 {
-	private Board[] boards;
-	private Player[] players;
+	private Board[] boards = new Board[2];
+	private Player[] players = new Player[2];
 	private ShotController shotCon;
 //	private Viewer display;
 	private Serializer serializer;
