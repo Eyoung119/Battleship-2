9ce44817f960f14b64c36a7b18e822076cbe5dbe
Eyoung88 <47370000+Eyoung88@users.noreply.github.com>
2019-02-28 14:03:50 -0700

    Add files via upload

---
+++ controller/Serializer.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import models.Board;
+
+public class Serializer {
+
+	private File file = new File(".txt");
+	
+	private void write(Board item) throws IOException {
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		try {
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+			fos = new FileOutputStream(file);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(item);
+		}
+		catch(Exception e) {
+			
+		}
+		finally {
+			fos.close();
+			oos.close();
+		}
+	}
+	
+	private Board read() throws IOException {
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		Board item = null;
+		try {
+			if(!file.exists()) {
+				write(new Board());
+			}
+			fis = new FileInputStream(file);
+			ois = new ObjectInputStream(fis);
+			item = (Board) ois.readObject();
+		}
+		catch(Exception e) {
+			
+		}
+		finally {
+			fis.close();
+			ois.close();
+		}
+		
+		return item;
+	}
+	
+}
