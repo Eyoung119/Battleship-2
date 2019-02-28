83cff7c8b3327dc24942b6609fc0e318c9041381
LoreleiLura <45364056+LoreleiLura@users.noreply.github.com>
2019-02-28 14:46:04 -0700

    Add files via upload

--- Viewer/Viewer.java
+++ Viewer/Viewer.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -20,28 +22,19 @@ public class Viewer extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 1500, 900);
-			//Scene p2=new Scene(root,1500,900);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
-			
-			primaryStage.setScene(scene);
-			Button lightSwitch = new Button("Don't Touch");
-			lightSwitch.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event) {
-					
-				}
-			});
-			VBox switchBox = new VBox();
-			switchBox.setAlignment(Pos.CENTER);
-			switchBox.setPadding(new Insets(20, 40, 20, 40));
-			switchBox.getChildren().add(lightSwitch);
-
-			root.getChildren().addAll(switchBox);
-			primaryStage.setTitle("BattleShip 2: More");
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("BattleShip-2.fxml"));
+		    
+	        Scene scene = new Scene(root, 300, 275);
+	    
+	        primaryStage.setTitle("FXML Welcome");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root, 1500, 900);
+//			//Scene p2=new Scene(root,1500,900);
+//			scene.getStylesheets().add(getClass().getResource("BattleShip-2.fxml").toExternalForm());
+//			primaryStage.setTitle("BattleShip 2: More");
+//			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 			Platform.exit();
