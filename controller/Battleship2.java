398e8e807f9de9535454ea619eed9106cff0bea0
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-02-28 14:51:14 -0700

    Add files via upload

---
+++ controller/Battleship2.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import models.Board;
+import models.Player;
+
+public class Battleship2 {
+	private Board[] boards;
+	private Player[] players;
+	private ShotController shotCon;
+//	private Viewer display;
+	private Serializer serializer;
+	
+	public void run() {
+		
+	}
+	
+	public void takeTurn() {
+		
+	}
+	
+	public void serialize() {
+		
+	}
+	
+	public void updateDisplay() {
+		
+	}
+}
