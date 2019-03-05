0b2bfeb4af6de8bdfe33bf269049d2003bcebb10
LoreleiLura <45364056+LoreleiLura@users.noreply.github.com>
2019-03-05 16:21:12 -0700

    Add files via upload

--- src/Viewer/BattleShipSceneBuilder.java
+++ src/Viewer/BattleShipSceneBuilder.java
@@ -140,6 +140,14 @@ public class BattleShipSceneBuilder {
 				}
 			}
 		}
+		Label P1Name=new Label(boardInput[0].getPlayer().getName());
+		P1Name.setMinSize(50, 50);
+		root.setConstraints(P1Name, 5, 14);
+		Label P2Name=new Label(boardInput[1].getPlayer().getName());
+		P1Name.setMinSize(50, 50);
+		root.setConstraints(P2Name, 20, 14);
+		root.getChildren().addAll(P1Name,P2Name);
+		
 		
 		root.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(root, 1700, 900);
