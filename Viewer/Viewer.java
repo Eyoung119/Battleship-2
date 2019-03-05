0f75ae581660c3e7dbadd3303dacaefb7dd6ff61
LoreleiLura <45364056+LoreleiLura@users.noreply.github.com>
2019-03-05 13:41:18 -0700

    Add files via upload

--- Viewer/Viewer.java
+++ Viewer/Viewer.java
@@ -2,39 +2,29 @@ package Viewer;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import models.*;
 
 public class Viewer extends Application {
-	public void run() {
+private BattleShipSceneBuilder BS=new BattleShipSceneBuilder();
+private static Board viewerBoard;
+
+
+//Create a viewer and pass in Board from run()
+//This will create a new window displaying the values
+	public void run(Board b) {
+		viewerBoard=b;
+		System.out.println(viewerBoard);
+		BS.passBoard(b);
 		launch("");
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("BattleShip-2.fxml"));
-		    
-	        Scene scene = new Scene(root, 300, 275);
-	    
-	        primaryStage.setTitle("FXML Welcome");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root, 1500, 900);
-//			//Scene p2=new Scene(root,1500,900);
-//			scene.getStylesheets().add(getClass().getResource("BattleShip-2.fxml").toExternalForm());
-//			primaryStage.setTitle("BattleShip 2: More");
-//			primaryStage.show();
+			primaryStage.setScene(BS.boardDisplayTest(viewerBoard));
+			primaryStage.setTitle("BattleShip 2: More");
+			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 			Platform.exit();
