077246aede0e464d6fac41ffd6f5574c55d01089
Eyoung88 <47370000+Eyoung88@users.noreply.github.com>
2019-03-05 16:17:55 -0700

    Update shotController.java

--- controller/shotController.java
+++ controller/shotController.java
@@ -1,5 +1,8 @@
 package controllers;
 
 public class shotController {
-
+  //class where assign the button to the type of weapon available per ship type.
+	//takes in?
+	//uses the scenebuilder of buttons in grid scene
+  
 }
