6e020ee64244b5722c0fd28b643f159df0d45978
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-03-05 15:02:32 -0700

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,33 +1,42 @@
 package application;
 
+import controllers.Battleship2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	public void run() {
-		launch("");
-	}
+	
+	private static Stage stage = new Stage();
+	private Battleship2 control = new Battleship2();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/viewer/GameGUI.fxml"));
+			stage = primaryStage;
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/viewer/GameGUI.fxml"));
+			loader.setController(control);
+			Parent root = loader.load();
+			
 		    
 	        Scene scene = new Scene(root, 359, 374);
 	    
-	        primaryStage.setTitle("Battleship 2: More");
+	        primaryStage.setTitle("Battleship 2: MORE");
+	        //BattleShipSceneBuilder.boardDisplayTest()
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public static Stage getStage() {
+		return stage;
 	}
 }
