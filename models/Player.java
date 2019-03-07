1f08946c21f2efa5792f254860219a54a9e04fed
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-03-07 14:13:13 -0700

    Update Player.java

--- models/Player.java
+++ models/Player.java
@@ -1,12 +1,19 @@
 package models;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Player implements Serializable {
 	private String name;
+	private HashMap<String, Ship> ships = new HashMap<String, Ship>();
 	
 	public Player(String name) {
 		setName(name);
+		ships.put("Carrier", new Ship(shipType.CARRIER));
+		ships.put("Battleship", new Ship(shipType.BATTLESHIP));
+		ships.put("Cruiser", new Ship(shipType.CRUISER));
+		ships.put("Submarine", new Ship(shipType.SUBMARINE));
+		ships.put("Destroyer", new Ship(shipType.DESTROYER));
 	}
 	
 	public String getName() {
@@ -15,5 +22,9 @@ public class Player implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public HashMap<String, Ship> getShips() {
+		return ships;
 	}
 }
