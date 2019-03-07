beabb76c93c33e4335fc1691aecd5700de16cc6d
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2019-03-07 14:26:31 -0700

    Update Cell.java

--- models/Cell.java
+++ models/Cell.java
@@ -3,6 +3,10 @@ package models;
 import java.io.Serializable;
 
 public class Cell implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private cellState state=cellState.EMPTY;
 //	SpriteMap sprite;
 	
