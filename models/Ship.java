3c00706b619452669e4ea11d23ac21bd647a12b2
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2019-03-07 14:28:35 -0700

    Update Ship.java

--- models/Ship.java
+++ models/Ship.java
@@ -3,6 +3,10 @@ package models;
 import java.io.Serializable;
 
 public class Ship implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private shipType type;
 	private boolean isSunk = false;
 	private int length;
