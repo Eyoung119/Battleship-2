81fa60e9d8be68af277643877a1eeff92cb1fc07
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2019-03-07 14:27:59 -0700

    Update Player.java

--- models/Player.java
+++ models/Player.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 public class Player implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private HashMap<String, Ship> ships = new HashMap<String, Ship>();
 	
