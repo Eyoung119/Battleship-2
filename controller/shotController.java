1d521f2db38230530a9958b0115bbc39b2074594
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-03-07 14:57:23 -0700

    Update shotController.java

--- controller/shotController.java
+++ controller/shotController.java
@@ -1,8 +1,54 @@
 package controllers;
 
-public class shotController {
-  //class where assign the button to the type of weapon available per ship type.
-	//takes in?
-	//uses the scenebuilder of buttons in grid scene
-  
+import models.Board;
+import models.Cell;
+import models.cellState;
+
+public class ShotController {
+	private Board[] boards;
+	private int currentPlayer = 0;
+	
+	public ShotController(Board[] boards) {
+		setBoards(boards);
+	}
+	
+	public void setBoards(Board[] boards) {
+		this.boards = boards;
+	}
+	
+	private void setCurrentPlayer(int currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+	
+	public void shot (int currentPlayer) {
+		setCurrentPlayer(currentPlayer);
+	}
+	
+	public boolean spyPlane (int currentPlayer, Cell[] cells) {
+		setCurrentPlayer(currentPlayer);
+		for (Cell c : cells) {
+			if (c.getCellState() == cellState.SHIP) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void mortarStrike (int currentPlayer) {
+		setCurrentPlayer(currentPlayer);
+	}
+	
+	public void missileBarrage (int currentPlayer) {
+		setCurrentPlayer(currentPlayer);
+	}
+	
+	public void sonar (int currentPlayer) {
+		setCurrentPlayer(currentPlayer);
+	}
+	
+	public void doubleShot (int currentPlayer) {
+		setCurrentPlayer(currentPlayer);
+		shot(currentPlayer);
+		shot(currentPlayer);
+	}
 }
