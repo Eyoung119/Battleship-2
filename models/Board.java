3f5709eab0ea2397677af828293e26a8d8acc8ff
Eyoung88 <47370000+Eyoung88@users.noreply.github.com>
2019-03-08 08:58:00 -0700

    Update Board.java

--- models/Board.java
+++ models/Board.java
@@ -3,9 +3,7 @@ package models;
 import java.io.Serializable;
 
 public class Board implements Serializable {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private Player player = null;
 	private Cell[][] cells = new Cell[10][10];
@@ -21,11 +19,13 @@ public class Board implements Serializable {
 	
 	public Cell[][] getCells() {
 		return cells;
+		
 	}
 	
 	public void placeShips() {
 		//This method is INCOMPLETE!!!
 		//When GUI is finished, complete logic for placing ships.
+		//Select space for ships?
 	}
 	
 	public Player getPlayer() {
@@ -35,4 +35,4 @@ public class Board implements Serializable {
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
-}
+} 
