14b0805338d6e5cba7bbd3a0c93999a11694dbf4
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-03-09 15:30:35 -0700

    Update Cell.java

--- models/Cell.java
+++ models/Cell.java
@@ -3,12 +3,8 @@ package models;
 import java.io.Serializable;
 
 public class Cell implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private cellState state=cellState.EMPTY;
-//	SpriteMap sprite;
+	private cellState state = cellState.EMPTY;
+	private Ship ship;
 	
 	public Cell() {
 		
@@ -20,5 +16,18 @@ public class Cell implements Serializable {
 	
 	public void setCellState(cellState state) {
 		this.state = state;
+	}
+	
+	public void setShip(Ship ship) {
+		this.ship = ship;
+	}
+	
+	public Ship getShip() {
+		return ship;
+	}
+	
+	@Override
+	public String toString() {
+		return state.toString();
 	}
 }
