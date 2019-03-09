73d4412ed5a9134bc7ecf544e3a4357df4e11e70
Eyoung88 <47370000+Eyoung88@users.noreply.github.com>
2019-03-09 15:33:21 -0700

    Update Cell.java

--- models/Cell.java
+++ models/Cell.java
@@ -3,6 +3,7 @@ package models;
 import java.io.Serializable;
 
 public class Cell implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private cellState state = cellState.EMPTY;
 	private Ship ship;
 	
