fc332cb1aab1d5b001d42bbb868a9433097741ef
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-03-10 13:44:20 -0600

    Update Battleship2.java

--- controller/Battleship2.java
+++ controller/Battleship2.java
@@ -75,111 +75,113 @@ public class Battleship2 {
 				try {
 					shotCon = new ShotController(boards);
 					
-					ArrayList<Character> letter = new ArrayList<Character>();
-					for (int i = 65; i < 75; i++) {
-						Character y = (char) i;
-						letter.add(y);
-					}
-					ArrayList<Integer> number = new ArrayList<Integer>();
-					for (int i = 1; i < 11; i++) {
-						number.add(i);
-					}
-					ArrayList<String> orientations = new ArrayList<String>();
-						orientations.add("Left");
-						orientations.add("Right");
-						orientations.add("Up");
-						orientations.add("Down");
-						
-					
-					ChoiceDialog<Character> dialog = new ChoiceDialog<>('A', letter);
-					int carrierX = 0;
-					dialog.setTitle("Carrier");
-					dialog.setHeaderText(null);
-					dialog.setContentText("Select the first cordinate for your carrier");
-					Optional<Character> result3 = dialog.showAndWait();
-					if(result3.isPresent()) {
-						carrierX = result3.get() - 64;
-					}
-					
-					ChoiceDialog<Integer> dialog2 = new ChoiceDialog<>(1, number);
-					int carrierY = 0;
-					dialog2.setTitle("Carrier");
-					dialog2.setHeaderText(null);
-					dialog2.setContentText("Select the first cordinate for your carrier");
-					Optional<Integer> result4 = dialog2.showAndWait();
-					if(result4.isPresent()) {
-						carrierX = result4.get();
-					}
-					
-					ChoiceDialog<String> dialog3 = new ChoiceDialog<>("Left", orientations);
-					dialog3.setTitle("Carrier");
-					dialog3.setHeaderText(null);
-					dialog3.setContentText("Select the first cordinate for your carrier");
-					boolean isValid;
-					String orientation = null;
-					do {
-						isValid = true;
-						Optional<String> result5 = dialog3.showAndWait();
-						if(result5.isPresent()) {
-							orientation = result5.get();
-							switch (orientation) {
-							case "Left":
-								if(carrierX - 4 < 0) {
-									isValid = false;
-								}
-								break;
-							case "Right":
-								if(carrierX + 4 < 0) {
-									isValid = false;
-								}
-								break;
-							case "Up":
-								if(carrierX - 4 < 0) {
-									isValid = false;
-								}
-								break;
-							case "Down":
-								if(carrierX + 4 < 0) {
-									isValid = false;
-								}
-								break;
-							}
-						}
-					
-					} while (!isValid);
-					carrierX--;
-					carrierY--;
+					ArrayList<Character> letters = new ArrayList<>();
+			for (int i = 65; i < 75; i++) {
+				Character y = (char) i;
+				letters.add(y);
+			}
+			ArrayList<Integer> numbers = new ArrayList<>();
+			for (int i = 1; i < 11; i++) {
+				numbers.add(i);
+			}
+			ArrayList<String> orientations = new ArrayList<>();
+			orientations.add("Left");
+			orientations.add("Right");
+			orientations.add("Up");
+			orientations.add("Down");
+			
+			ChoiceDialog<Character> dialog = new ChoiceDialog<>('A', letters);
+			int carrierX = 0;
+			dialog.setTitle("Carrier");
+			dialog.setHeaderText(null);
+			dialog.setContentText("Select the first coordinate for your carrier:");
+			Optional<Character> result3 = dialog.showAndWait();
+			if (result3.isPresent()) {
+				carrierX = result3.get() - 64;
+			}
+			
+			ChoiceDialog<Integer> dialog2 = new ChoiceDialog<>(1, numbers);
+			int carrierY = 0;
+			dialog2.setTitle("Carrier");
+			dialog2.setHeaderText(null);
+			dialog2.setContentText("Select the second coordinate for your carrier:");
+			Optional<Integer> result4 = dialog2.showAndWait();
+			if (result4.isPresent()) {
+				carrierY = result4.get();
+			}
+			
+			ChoiceDialog<String> dialog3 = new ChoiceDialog<>("Left", orientations);
+			dialog3.setTitle("Carrier");
+			dialog3.setHeaderText(null);
+			dialog3.setContentText("Select the orientation for your carrier:");
+			boolean isValid;
+			String orientation = null;
+			do {
+				isValid = true;
+				Optional<String> result5 = dialog3.showAndWait();
+				if (result5.isPresent()) {
+					orientation = result5.get();
 					switch (orientation) {
-					case "Left":
-						for (int i = 0; i < 4; i++) {
-							boards[0].getCells()[carrierX - i][carrierY].setCellState(cellState.SHIP);
-							boards[0].getCells()[carrierX - i][carrierY].setShip(players[0].getShips().get("Carrier"));
-						}
-						break;
-					case "Right":
-						for (int i = 0; i < 4; i++) {
-							boards[0].getCells()[carrierX + i][carrierY].setCellState(cellState.SHIP);
-							boards[0].getCells()[carrierX + i][carrierY].setShip(players[0].getShips().get("Carrier"));
-						}
-						break;
-					case "Up":
-						for (int i = 0; i < 4; i++) {
-							boards[0].getCells()[carrierX][carrierY - i].setCellState(cellState.SHIP);
-							boards[0].getCells()[carrierX][carrierY - i].setShip(players[0].getShips().get("Carrier"));
-						}
-						break;
-					case "Down":
-						for (int i = 0; i < 4; i++) {
-							boards[0].getCells()[carrierX][carrierY + i].setCellState(cellState.SHIP);
-							boards[0].getCells()[carrierX][carrierY + i].setShip(players[0].getShips().get("Carrier"));
-						}
-						break;
-					
-					}	
-					viewer.run(boards);
-				} catch (Exception e) {
-					e.printStackTrace();
+						case "Left":
+							if (carrierX - 4 < 1) {
+								isValid = false;
+							}
+//							for (int x = 0; x < 5; x++) {
+//								if (boards[0].getCells()[carrierX - x][carrierY].getCellState() == cellState.SHIP) {
+//									isValid = false;
+//								}
+//							}
+							break;
+						case "Right":
+							if (carrierX + 4 > 10) {
+								isValid = false;
+							}
+							break;
+						case "Up":
+							if (carrierY - 4 < 1) {
+								isValid = false;
+							}
+							break;
+						case "Down":
+							if (carrierY + 4 > 10) {
+								isValid = false;
+							}
+							break;
+					}
 				}
+			} while (!isValid);
+			carrierX--;
+			carrierY--;
+			switch (orientation) {
+			case "Left":
+				for (int i = 0; i < 5; i++) {				
+					boards[0].getCells()[carrierX - i][carrierY].setCellState(cellState.SHIP);
+					boards[0].getCells()[carrierX - i][carrierY].setShip(players[0].getShips().get("Carrier"));
+				}
+				break;
+			case "Right":
+				for (int i = 0; i < 5; i++) {				
+					boards[0].getCells()[carrierX + i][carrierY].setCellState(cellState.SHIP);
+					boards[0].getCells()[carrierX + i][carrierY].setShip(players[0].getShips().get("Carrier"));
+				}
+				break;
+			case "Up":
+				for (int i = 0; i < 5; i++) {				
+					boards[0].getCells()[carrierX][carrierY - i].setCellState(cellState.SHIP);
+					boards[0].getCells()[carrierX][carrierY - i].setShip(players[0].getShips().get("Carrier"));
+				}
+				break;
+			case "Down":
+				for (int i = 0; i < 5; i++) {				
+					boards[0].getCells()[carrierX][carrierY + i].setCellState(cellState.SHIP);
+					boards[0].getCells()[carrierX][carrierY + i].setShip(players[0].getShips().get("Carrier"));
+				}
+				break;
+			}
+			viewer.run(boards);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 			}
 		}
 	}
