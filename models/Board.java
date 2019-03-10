3a0f3747f2185437cf1e95ccd418b2bdb568c111
dv-74 <48069827+dv-74@users.noreply.github.com>
2019-03-10 16:44:09 -0600

    Update Board.java

--- models/Board.java
+++ models/Board.java
@@ -4,28 +4,39 @@ import java.io.Serializable;
 
 public class Board implements Serializable {
 	
-	private static final long serialVersionUID = 1L;
-	private Player player = null;
+	private Player player;
 	private Cell[][] cells = new Cell[10][10];
+	private Cell[][] filter = new Cell[10][10];
+	private int turn = 0;
 	
 	public Board(Player player) {
 		setPlayer(player);
 		for (int x = 0; x < cells.length; x++) {
 			for (int y = 0; y < cells[x].length; y++) {
 				cells[x][y] = new Cell();
+				filter[x][y] = new Cell();
 			}
 		}
 	}
 	
 	public Cell[][] getCells() {
 		return cells;
-		
 	}
 	
+	public Cell[][] getFilter() {
+		return filter;
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public void setTurn(int turn) {
+		this.turn = turn;
+	}
 	public void placeShips() {
 		//This method is INCOMPLETE!!!
 		//When GUI is finished, complete logic for placing ships.
-		//Select space for ships?
 	}
 	
 	public Player getPlayer() {
@@ -35,4 +46,4 @@ public class Board implements Serializable {
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
-} 
+}
